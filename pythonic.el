8d1f2183b56d4fd3f7b50f90516822095c0974a8
Malyshev Artem <proofit404@gmail.com>
2015-01-22 12:25:54 +0300

    Write package header.

---
+++ pythonic.el
@@ -0,0 +1,31 @@
+;;; pythonic.el --- Utility functions for writing pythonic emacs package.
+
+;; Copyright (C) 2015 by Artem Malyshev
+
+;; Author: Artem Malyshev <proofit404@gmail.com>
+;; URL: https://github.com/proofit404/pythonic
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See the README for more details.
+
+;;; Code:
+
+(provide 'pythonic)
+
+;;; pythonic.el ends here
