804522e92e09ab80577f25f4045e3ba604b1faa9
Malyshev Artem <proofit404@gmail.com>
2015-01-23 14:00:54 +0300

    Wrote some tests.

--- test/pythonic-test.el
+++ test/pythonic-test.el
@@ -7,6 +7,26 @@
 (require 'ert)
 (require 'pythonic)
 
+(ert-deftest test-pythonic-executable ()
+  "Check python executable detection."
+  (should (s-equals? "python" (pythonic-executable))))
+
+(ert-deftest test-pythonic-executable-virtualenv ()
+  "Check python executable detection."
+  (let ((python-shell-virtualenv-path "/home/me/env"))
+    (should (s-equals? "/home/me/env/bin/python" (pythonic-executable)))))
+
+(ert-deftest test-pythonic-executable-windows ()
+  "Check python executable detection on windows platform."
+  (let ((system-type 'windows-nt))
+    (should (s-equals? "pythonw" (pythonic-executable)))))
+
+(ert-deftest test-pythonic-executable-windows-virtualenv ()
+  "Check python executable detection on windows platform."
+  (let ((system-type 'windows-nt)
+        (python-shell-virtualenv-path "C:/env"))
+    (should (s-equals? "C:/env/Scripts/pythonw" (pythonic-executable)))))
+
 (provide 'pythonic-test)
 
 ;;; pythonic-test.el ends here
