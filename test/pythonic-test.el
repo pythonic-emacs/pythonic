054401f3288e4c7d4f1069df9f62416507e25af6
Malyshev Artem <proofit404@gmail.com>
2015-01-23 13:34:45 +0300

    Initialize test runner.

---
+++ test/pythonic-test.el
@@ -0,0 +1,12 @@
+;;; pythonic-test.el --- pythonic test suite
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+(require 'pythonic)
+
+(provide 'pythonic-test)
+
+;;; pythonic-test.el ends here
---
+++ test/test-helper.el
@@ -0,0 +1,17 @@
+;;; test-helper.el --- ert-runner test helper
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cask)
+
+(let ((source-directory (locate-dominating-file load-file-name "Cask")))
+  (cask-initialize source-directory)
+  (add-to-list 'load-path source-directory))
+
+(undercover "*.el")
+
+(provide 'test-helper)
+
+;;; test-helper.el ends here
