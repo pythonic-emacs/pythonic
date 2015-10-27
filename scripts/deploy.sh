bf2a7d2d9ef2278ac06159319cf2f653d5eb2e9b
Artem Malyshev <proofit404@gmail.com>
2015-10-27 14:12:52 +0300

    Create tramp user correctly.
    Add host to authenticated host in the tramp user directory.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,22 @@
 #!/bin/bash -e
 
+HOST_USER=vagrant
+HOST_HOME=/home/$HOST_USER
+TRAMP_USER=test
+TRAMP_HOME=/home/$TRAMP_USER
+
 # Create tramp user.
 
-adduser test
-sudo -u test mkdir -p /home/test/.ssh
-cat $HOME/.ssh/id_rsa.pub | sudo -u test cat > /home/test/.ssh/authorized_keys
+adduser --quiet $TRAMP_USER
 
 # Register tramp host.
 
-ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ''
-touch $HOME/.ssh/known_hosts
-ssh-keygen -R localhost
-ssh-keyscan -H localhost > $HOME/.ssh/known_hosts
+sudo -u $HOST_USER ssh-keygen -t rsa -b 4096 -f $HOST_HOME/.ssh/id_rsa -N ''
+sudo -u $HOST_USER touch $HOST_HOME/.ssh/known_hosts
+sudo -u $HOST_USER ssh-keygen -R localhost
+sudo -u $HOST_USER ssh-keyscan -H localhost > $HOST_HOME/.ssh/known_hosts
+
+# Authorize localhost for tramp user.
+
+sudo -u $TRAMP_USER mkdir -p $TRAMP_HOME/.ssh
+sudo -u $HOST_USER cat $HOST_HOME/.ssh/id_rsa.pub | sudo -u $TRAMP_USER cat > $TRAMP_HOME/.ssh/authorized_keys
