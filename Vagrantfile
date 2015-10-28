e54ff92f74daa4edc705c0cccc81f0322ddc4187
Artem Malyshev <proofit404@gmail.com>
2015-10-28 17:21:37 +0300

    Run deploy from not privileged user.

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,9 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "hashicorp/precise32"
 
-  config.vm.provision "shell", path: "scripts/deploy.sh"
+  config.vm.provision "shell" do |s|
+    s.path = "scripts/deploy.sh"
+    s.privileged = false
+    s.keep_color = true
+  end
 end
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,39 +1,39 @@
 #!/bin/bash -e
 
-HOST_USER=vagrant
-HOST_HOME=/home/$HOST_USER
-TRAMP_USER=test
-TRAMP_HOME=/home/$TRAMP_USER
+PROJECT_ROOT=/vagrant
 
 # Create tramp user.
 
-adduser --quiet $TRAMP_USER
+TRAMP_USER=test
+TRAMP_HOME=/home/$TRAMP_USER
+
+sudo adduser --quiet $TRAMP_USER
 
 # Register tramp host.
 
-sudo -u $HOST_USER ssh-keygen -t rsa -b 4096 -f $HOST_HOME/.ssh/id_rsa -N ''
-sudo -u $HOST_USER touch $HOST_HOME/.ssh/known_hosts
-sudo -u $HOST_USER ssh-keygen -R localhost
-sudo -u $HOST_USER ssh-keyscan -H localhost > $HOST_HOME/.ssh/known_hosts
+ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/id_rsa -N ''
+touch $HOME/.ssh/known_hosts
+ssh-keygen -R localhost
+ssh-keyscan -H localhost > $HOME/.ssh/known_hosts
 
 # Authorize localhost for tramp user.
 
 sudo -u $TRAMP_USER mkdir -p $TRAMP_HOME/.ssh
-sudo -u $HOST_USER cat $HOST_HOME/.ssh/id_rsa.pub | sudo -u $TRAMP_USER cat > $TRAMP_HOME/.ssh/authorized_keys
+sudo -u $TRAMP_USER sh -c "echo '$(cat $HOME/.ssh/id_rsa.pub)' > $TRAMP_HOME/.ssh/authorized_keys"
 
 # Update mirrors list.
 
-apt-get update
+sudo apt-get update
 
 # Install Emacs build dependencies.
 
-apt-get install -y libncurses-dev libxpm-dev libxaw7-dev \
-                   libtiff4-dev libpng-dev libgif-dev autoconf \
-                   automake texinfo make
+sudo apt-get install -y libncurses-dev libxpm-dev libxaw7-dev \
+                        libtiff4-dev libpng-dev libgif-dev autoconf \
+                        automake texinfo make
 
 # Build Emacs.
 
-EMACS_DIR=/usr/local/emacs
+EMACS_DIR=$HOME/.emacs
 EMACS_SRC=$EMACS_DIR/src
 EMACS_VERSIONS=(emacs-24.3 emacs-24.4 emacs-24.5)
 
