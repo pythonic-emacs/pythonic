a131989a1567cb0164ea223b6f8669bb14996d22
Artem Malyshev <proofit404@gmail.com>
2018-07-27 12:09:03 +0300

    Add developer note.

--- pythonic.el
+++ pythonic.el
@@ -202,6 +202,9 @@ print(json.dumps(yaml.safe_load(open(sys.argv[-1], 'r'))))
         (let* ((filename (pythonic-get-docker-compose-filename project))
                (struct (pythonic-read-docker-compose-file filename))
                (volumes (pythonic-get-docker-compose-volumes struct))
+               ;; FIXME: Each service can have many volumes.  It
+               ;; should appears once in the selection and all volumes
+               ;; should be added to the alias list.
                (volume (if (< 1 (length volumes))
                            (assoc (completing-read "Service: " (mapcar 'car volumes) nil t) volumes)
                          (car volumes)))
